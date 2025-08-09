83b2a9bc2d3c958a8e76eca7c556bb5be239c721
Victor C. Vivas N. <victorc.vivas@gmail.com>
2025-08-08 21:38:46 -0500

    cambios header footer y validacion

--- script14.js
+++ script14.js
@@ -9,11 +9,11 @@ function validarFormulario() {
     
     // Obtener los valores de los campos del formulario
   const nombre = document.getElementById('nombre').value.trim();
-  const email = document.getElementById('email').value.trim();
+  const email = document.getElementById('email').value;
   const mensaje = document.getElementById('mensaje').value.trim();
     
-    // Validar que los campos no estén vacíos   
-    if(nombre === '' || email === '' || mensaje === '') { 
+    // Validar que los campos no estén vacíos e email tenga formato correcto
+    if(nombre === '' || email === '' || mensaje === ''|| !email.includes("@") || !email.includes(".")) { 
         alert('Por favor, complete todos los campos.');
         
     }else{
